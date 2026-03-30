b9db2c8a6fceca7c5b39f7a4b531a49c90e2f37b
ILIA <ikpolushin@yandex.ru>
2026-03-30 19:56:11 +0300

    test anonymous user can see a new

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -1,15 +1,40 @@
 from http import HTTPStatus
+from datetime import datetime
 
 import pytest
 from django.urls import reverse
 
+from news.models import News
+
+@pytest.fixture
+def new():
+    new = News.objects.create(
+        title = 'new_title',
+        text = 'new_text',
+        date = datetime.today(),
+    )
+
+    return new
+
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     'url_name',
-    ('news:home', 'users:login', 'users:signup', 'users:logout')
+    (
+        'news:home',
+        'news:detail',
+        'users:login',
+        'users:signup',
+        'users:logout'
+    )
 )
-def test_home_availability_for_anonymous_user(client, url_name):
-    url = reverse(url_name)
+def test_home_availability_for_anonymous_user(client, url_name, new):
+
+    if url_name == 'news:detail':
+        url = reverse(url_name, args=(new.pk,))
+    else:
+        url = reverse(url_name)
+
+
     if url_name == 'users:logout':
         response = client.post(url)
     else:
