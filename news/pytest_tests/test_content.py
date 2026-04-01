f63131ea550fffe7f5db1125ebf11124f29182f3
ILIA <ikpolushin@yandex.ru>
2026-04-01 22:24:51 +0300

    test home display required amount of news and has correct order

--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -1,7 +1,8 @@
 import pytest
-from datetime import datetime, timedelta
+from datetime import timedelta, date
 
 from django.urls import reverse
+from pytest_django.fixtures import client
 
 from news.models import News
 from yanews import settings
@@ -10,22 +11,28 @@ NEWS_AMOUNT = 16
 
 @pytest.fixture
 def news_creation():
-
-    current_date = datetime.today()
+    current_date = date.today()
 
     news_list = [
         News(
             title=f'new_title_{i}',
             text=f'new_text_{i}',
-            date=current_date - timedelta(days=1),
+            date=current_date - timedelta(days=i),
         )
-        for i in range(1, NEWS_AMOUNT)
+        for i in range(NEWS_AMOUNT)
     ]
-
     News.objects.bulk_create(news_list)
 
 @pytest.mark.django_db
-def test_new_amount():
-    response = reverse('news:home')
+def test_news_amount_and_order(client, news_creation):
+    url = reverse('news:home')
+    response = client.get(url)
     object_list = response.context['object_list']
     assert len(object_list) == settings.NEWS_COUNT_ON_HOME_PAGE
+
+    dates_from_page = [new.date for new in object_list]
+    current_date = date.today()
+    expected_dates = [current_date - timedelta(days=i) for i in range(NEWS_AMOUNT)]
+    expected_dates = sorted(expected_dates, reverse=True)[:settings.NEWS_COUNT_ON_HOME_PAGE]
+
+    assert dates_from_page == expected_dates
