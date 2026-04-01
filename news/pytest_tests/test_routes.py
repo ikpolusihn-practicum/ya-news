08e20333c56c617ad333be808c775671b541538f
ikpolushin <ikpolushin@yandex.ru>
2026-04-01 19:53:09 +0300

    test for comment and delete pages availability

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import pytest
 from django.urls import reverse
 from django.test.client import Client
+from pytest_django.asserts import assertRedirects
 
 from news.models import News, Comment
 
@@ -19,7 +20,11 @@ def other_user(django_user_model):
 def comment_author_client(comment_author):
     client = Client()
     client.force_login(comment_author)
-    return comment_author_client
+    return client
+
+@pytest.fixture
+def anonymous_user_client():
+    return Client()
 
 @pytest.fixture
 def other_user_client(other_user):
@@ -74,32 +79,31 @@ def test_home_availability_for_anonymous_user(client, url_name, new):
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    'url_name, client_fixture, expected_status, expected_behavior',
+    'url_name, client_fixture, expected_status, scenario',
     [
-        # Author can access edit/delete
-        ('news:edit', pytest.lazy_fixture('comment_author_client'), HTTPStatus.OK, 'access'),
-        ('news:delete', pytest.lazy_fixture('comment_author_client'), HTTPStatus.OK, 'access'),
+        ('news:edit', pytest.lazy_fixture('comment_author_client'), HTTPStatus.OK, 'comment_author'),
+        ('news:delete', pytest.lazy_fixture('comment_author_client'), HTTPStatus.OK, 'comment_author'),
 
-        # Other user cannot access edit/delete (404 not found)
-        ('news:edit', pytest.lazy_fixture('other_user_client'), HTTPStatus.NOT_FOUND, 'forbidden'),
-        ('news:delete', pytest.lazy_fixture('other_user_client'), HTTPStatus.NOT_FOUND, 'forbidden'),
+        ('news:edit', pytest.lazy_fixture('other_user_client'), HTTPStatus.NOT_FOUND, 'other_user'),
+        ('news:delete', pytest.lazy_fixture('other_user_client'), HTTPStatus.NOT_FOUND, 'other_user'),
 
-        # Anonymous user is redirected to login
-        # ('news:edit', pytest.lazy_fixture('anonymous_client'), HTTPStatus.FOUND, 'redirect'),
-        # ('news:delete', pytest.lazy_fixture('anonymous_client'), HTTPStatus.FOUND, 'redirect'),
+        ('news:edit', pytest.lazy_fixture('anonymous_user_client'), HTTPStatus.FOUND, 'anonymous_user'),
+        ('news:delete', pytest.lazy_fixture('anonymous_user_client'), HTTPStatus.FOUND, 'anonymous_user'),
     ]
 )
-def test_comment_edit_and_delete_access_comprehensive(
+def test_comment_and_delete_availability(
         comment,
         url_name,
         client_fixture,
         expected_status,
-        expected_behavior
+        scenario,
 ):
     url = reverse(url_name, args=(comment.pk,))
     response = client_fixture.get(url)
 
     assert response.status_code == expected_status
 
-    if expected_behavior == 'redirect':
-        assert 'login' in response.url
+    if scenario == 'anonymous_user':
+        login_url = reverse('users:login')
+        redirect_url = f'{login_url}?next={url}'
+        assertRedirects(response, redirect_url)
