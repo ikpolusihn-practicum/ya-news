4003b04d0a74d94e6d5fb19661450ae90da77466
ILIA <ikpolushin@yandex.ru>
2026-04-02 09:55:14 +0300

    test comment creation

---
+++ news/pytest_tests/test_logic.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+import pytest
+from django.test.client import Client
+from django.urls import reverse
+
+from news.models import News, Comment
+
+
+@pytest.fixture
+def authorized_user(django_user_model):
+    return django_user_model.objects.create(username='authorized_user')
+
+@pytest.fixture
+def authorized_user_client(authorized_user):
+    client = Client()
+    client.force_login(authorized_user)
+    return client
+
+@pytest.fixture
+def anonymous_user_client():
+    return Client()
+
+@pytest.fixture
+def new():
+    new = News.objects.create(
+        title = 'new_title',
+        text = 'new_text',
+        date = datetime.today(),
+    )
+
+    return new
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'scenario, client_fixture, expected_comments_count',
+    (
+        ('authorized_user', pytest.lazy_fixture('authorized_user_client'), 1),
+        ('unauthorized_user', pytest.lazy_fixture('anonymous_user_client'), 0),
+    )
+)
+def test_comment_creation(new, scenario, client_fixture, expected_comments_count):
+    url = reverse('news:detail', args=(new.pk,))
+    comment_data = {
+        'text': 'comment_text',
+    }
+    client_fixture.post(url, comment_data)
+    comment_count = Comment.objects.filter(news=new.pk).count()
+    if scenario == 'authorized_user':
+        assert comment_count == 1
+    elif scenario == 'unauthorized_user':
+        assert comment_count == 0
